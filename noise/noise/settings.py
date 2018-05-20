50985c6582a338ccbe09d45b309ce4debf4d3d38
Beverly Pham <beverly.pham@gmail.com>
2018-05-20 01:50:37 -0700

    install coverage

--- noise/noise/settings.py
+++ noise/noise/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coverage',
     'noise',
     'noise_profile',
     'sass_processor',
