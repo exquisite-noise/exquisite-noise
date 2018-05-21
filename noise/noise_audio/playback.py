c6bbe3babce1bab522db6f6d5954b8437c6c1a66
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-05-21 15:11:08 -0700

    added function to playback files

---
+++ noise/noise_audio/playback.py
@@ -0,0 +1,20 @@
+import pygame as pg
+
+
+def play_music2(music_file, volume=0.8):
+    freq = 44100
+    bitsize = -16
+    channels = 2
+    buffer = 2048
+    pg.mixer.init(freq, bitsize, channels, buffer)
+    pg.mixer.music.set_volume(volume)
+    clock = pg.time.Clock()
+    try:
+        pg.mixer.music.load(music_file)
+        print("Music file {} loaded!".format(music_file))
+    except pg.error:
+        print("File {} not found! ({})".format(music_file, pg.get_error()))
+    return
+    pg.mixer.music.play()
+    while pg.mixer.music.get_busy():
+        clock.tick(30)
--- noise/noise_audio/views.py
+++ noise/noise_audio/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from pydub import AudioSegment
+import os
 
 # Create your views here.
