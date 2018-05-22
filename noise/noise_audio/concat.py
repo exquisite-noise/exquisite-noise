4b6511988dba43fd41bef0e30f7c715ef236af63
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-05-21 19:34:08 -0700

    commented out broken code in concat.py

---
+++ noise/noise_audio/concat.py
@@ -0,0 +1,10 @@
+import os
+
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from pydub import AudioSegment
+
+
+# def concat_clips(*args):
+#     for i in args:
+#         i + args
