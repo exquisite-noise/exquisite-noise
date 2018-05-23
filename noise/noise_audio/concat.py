cb9c0edaf81b9272263e4e154ddfa724accdab25
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-05-23 11:49:02 -0700

    still working on concatenating files

--- noise/noise_audio/concat.py
+++ noise/noise_audio/concat.py
@@ -10,13 +10,13 @@ def concat_clips(prev_clip, new_clip):
     Concat previous clip with new clip.
     """
     import pdb; pdb.set_trace()
-    new = AudioSegment.from_mp3(new_clip)
     prev = AudioSegment.from_file(prev_clip)
+    new = AudioSegment.from_file(new_clip)
 
     combo = prev + new
 
     combined_clip = combo.export(
-        os.path.join(settings.MEDIA_ROOT, combo), format='mp3'
+        os.path.expanduser(settings.MEDIA_ROOT), format='mp3'
     )
 
     new_combined_clip = SimpleUploadedFile(
